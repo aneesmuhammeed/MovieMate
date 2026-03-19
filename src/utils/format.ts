debf077696d4971f39b8df9453fb1936fd0d3d32
anees <aneesk002@gmail.com>
2026-03-19 11:09:56 +0530

    App to render only HeroHeader component

---
+++ src/utils/format.ts
@@ -0,0 +1,11 @@
+export function formatDate(isoDate: string): string {
+  const date = new Date(isoDate)
+  if (Number.isNaN(date.getTime())) {
+    return isoDate
+  }
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  }).format(date)
+}
